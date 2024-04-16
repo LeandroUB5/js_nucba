18bf3785011f6f26ab98b89786abe86680560247
Leandro <leandrobandini01@gmail.com>
2024-04-16 15:49:11 -0300

    Edit pag

--- js_ejercicios/js_ejercicio_1/script.js
+++ js_ejercicios/js_ejercicio_1/script.js
@@ -1,4 +1,4 @@
 var nombre = prompt("Escribe tu nombre");
-var edad = prompt("Escriba su edad");
+var edad = parseInt(prompt("Escriba su edad"));
 
 console.log("Hola! Mi nombre es " + nombre + " y tengo " + edad + " años");
