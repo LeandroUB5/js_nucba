f94136bc2c8684a2c0563e6d720b113bb44fb300
Leandro <leandrobandini01@gmail.com>
2024-04-24 16:12:12 -0300

    Clase 3

---
+++ js_clases/js_clase_3/script.js
@@ -0,0 +1,47 @@
+///////////////// for ////////////////
+
+// for (let i = 1; i <= 10; i++) {
+//   //console.log(`El valor actual de i es ${i}`);
+//   let nombre = prompt("Ingrese su nombre");
+//   if (nombre == "pepe") {
+//     console.log("pepe, vos estas acomodado");
+//     continue;
+//   }
+//   console.log(`Usted es el estudiante Nº ${i} Adelante, ya esta inscripto`);
+// }
+// console.log("Fin de las inscripciones");
+
+///////////////// arrays ////////////////
+
+let alumno1 = "hector";
+let alumno2 = "jonnhy";
+let alumno3 = "lucre";
+
+let alumnos = ["hector", "jonnhy", "lucre", "gabriel", "alan", "pedro"];
+// posiciones       0       1       2           3          4
+// console.log(alumnos);
+// console.log(alumnos[0]);
+// console.log(alumnos[1]);
+// console.log(alumnos[2]);
+// console.log(alumnos[3]);
+// console.log(alumnos[4]);
+
+// for (let i = 0; i < alumnos.length; i++) {
+//   // .length te toma todos los elementos que tiene el array
+//   console.log(alumnos[i]);
+// }
+
+//For of
+
+// for (let alumno of alumnos) {
+//   console.log(alumno);
+// }
+///////////////// while ////////////////
+
+// let ahorros = 0;
+// let objetivos = 5000;
+// while (ahorros < objetivos) {
+//   ahorros += 1000;
+//   console.log(`Ahorros actuales ${ahorros}`);
+// }
+// console.log(`Felicidades, lograste tu objetivo`);
